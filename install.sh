db8b55924b91cc81e387c18f6f0515fcdb69c0a8
iameskild <eskild.eriksen122@gmail.com>
2024-01-03 09:43:23 -0800

    add mamba to install script

--- install.sh
+++ install.sh
@@ -8,3 +8,15 @@ if ! command -v brew >/dev/null 2>&1; then
 else
   echo "Homebrew is already installed."
 fi
+
+
+MINIFORGE_INSTALLER="Miniforge3-Darwin-arm64.sh"
+
+if ! command -v mamba >/dev/null 2>&1 ; then
+  echo "Downloading Miniforge installer..."
+  curl -L -O "https://github.com/conda-forge/miniforge/releases/latest/download/$MINIFORGE_INSTALLER"
+  chmod +x "$MINIFORGE_INSTALLER"
+  ./"$MINIFORGE_INSTALLER"
+else
+  echo "Mamba is already installed."
+fi
